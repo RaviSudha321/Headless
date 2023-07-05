732f4a491cc0cadae97b7a95c7ecfada1aa45a0e
Ravi Sudha <ravisudha.610weblab@gmail.com>
2023-07-05 12:19:31 +0530

    Load More functionality added on the blogs page

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -4,6 +4,7 @@ import InnerBanner from '@/Components/InnerBanner/InnerBanner';
 import Link from 'next/link';
 import Date from '@/Components/Date';
 import { getAllPosts } from '../../../lib/posts';
+import { useState } from 'react';
 
 // export async function getStaticProps(){
 //     const response = await fetch('https://610weblab.in/headless/wp-json/wp/v2/posts?_embed&page=1');
@@ -28,6 +29,49 @@ export async function getStaticProps(){
 
 function Blogs({posts}){
 
+    const [allPosts, setAllPosts] = useState(posts);
+    const [loadButtonText, setLoadButtonText] = useState('Load More');
+    const [buttonDisbaled, setButtonDisabled] = useState(false);
+    const [endCursor, setEndCursor] = useState(allPosts.pageInfo.endCursor);
+
+    const handleLoadMore = async (e) => {
+        e.preventDefault();
+
+        setLoadButtonText('Loading...');
+      
+        const morePosts = await getAllPosts(endCursor);
+
+        let updatedPosts = {
+            pageInfo: {
+
+            },
+            nodes : []
+        }
+
+        updatedPosts.pageInfo = morePosts.pageInfo;
+
+        allPosts.nodes.map((node)=>{
+            updatedPosts.nodes.push(node);
+        });
+
+        morePosts.nodes.map((node)=>{
+            updatedPosts.nodes.push(node);
+        });
+
+        setEndCursor(morePosts.pageInfo.endCursor)
+
+        setAllPosts(updatedPosts);
+
+        if(allPosts.pageInfo.hasNextPage == false){
+            setLoadButtonText('No more posts available');
+            setButtonDisabled(true);
+        } else {
+            setLoadButtonText('Load More');
+            setButtonDisabled(false);
+        }
+
+    }
+
     return(
         <>
         <Header />
@@ -36,7 +80,7 @@ function Blogs({posts}){
             <div className='container'>
                 <div className='posts_list'>
                     {
-                       posts.nodes.map((data, index) => {
+                       allPosts.nodes.map((data, index) => {
                             return(
                                 <>
                                     <div className='post_item' key={index}>
@@ -60,36 +104,39 @@ function Blogs({posts}){
                                 </>
                             )
                         })
-
-
-                        // posts.nodes.map((data, index) => {
-                        //     return(
-                        //         <>
-                        //             <div className='post_item'>
-                        //                 <div className='post_img'>
-                        //                     {/* <img src={data._embedded['wp:featuredmedia'][0].source_url} alt={data._embedded['wp:featuredmedia'][0].title.rendered} /> */}
-                        //                 </div>
-                        //                 <div className='post_content'>
-                        //                     <h3 className='post_title'>
-                        //                         <Link href={`/blogs/${data.id}`}>{data.title.rendered}</Link>
-                        //                     </h3>
-                        //                     <div className='post_meta'>
-                        //                         <span className='author'><Link href={data._embedded.author[0].link}>{data._embedded.author[0].name}</Link></span>
-                        //                         <span className='date'><Date postDate={data.date} /></span>
-                        //                     </div>
-                        //                     <div className='post_excert' dangerouslySetInnerHTML={{__html: data.excerpt.rendered}}></div>
-                        //                     <div className='post_btn'>
-                        //                         <Link href={`/blogs/${data.id}`}>Read More</Link>
-                        //                     </div>
-                        //                 </div>
-                        //             </div>
-                        //         </>
-                        //     )
-                        // })
                     }
+                </div>
+                <div className='global_btn load_more_btn'>
+                    <a href="#" onClick={handleLoadMore} className={buttonDisbaled ? 'disabled' : null}>{loadButtonText}</a>
                 </div>
             </div>
         </section>
+        {
+            // posts.nodes.map((data, index) => {
+            //     return(
+            //         <>
+            //             <div className='post_item'>
+            //                 <div className='post_img'>
+            //                     {/* <img src={data._embedded['wp:featuredmedia'][0].source_url} alt={data._embedded['wp:featuredmedia'][0].title.rendered} /> */}
+            //                 </div>
+            //                 <div className='post_content'>
+            //                     <h3 className='post_title'>
+            //                         <Link href={`/blogs/${data.id}`}>{data.title.rendered}</Link>
+            //                     </h3>
+            //                     <div className='post_meta'>
+            //                         <span className='author'><Link href={data._embedded.author[0].link}>{data._embedded.author[0].name}</Link></span>
+            //                         <span className='date'><Date postDate={data.date} /></span>
+            //                     </div>
+            //                     <div className='post_excert' dangerouslySetInnerHTML={{__html: data.excerpt.rendered}}></div>
+            //                     <div className='post_btn'>
+            //                         <Link href={`/blogs/${data.id}`}>Read More</Link>
+            //                     </div>
+            //                 </div>
+            //             </div>
+            //         </>
+            //     )
+            // })
+        }
         <Footer />
         </>
     )
